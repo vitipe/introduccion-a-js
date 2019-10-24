d00e268191972abe800dbbdd90ee896470995951
vitipe <victorcasbas@gmail.com>
2019-10-24 15:46:23 -0300

    Completando función calcular

--- tareas/tarea-6/tarea-6-tarea2/tarea-6-tarea2.js
+++ tareas/tarea-6/tarea-6-tarea2/tarea-6-tarea2.js
@@ -9,39 +9,94 @@ Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como
 
 //Funciones que van a ser utilizadas:
 
-function calcularMayorSalario() {
-
+function calcularMayorSalarioAnual(arraySalarios) {
+    let mayorSalario = arraySalarios[0];
+    for (let i = 0; i < arraySalarios.length; i++) {
+        if (arraySalarios[i] > mayorSalario) {
+            mayorSalario = arraySalarios[i];
+        }
+    }
+    return mayorSalario;
 }
 
-function calcularMenorSalario() {
-
+function calcularMenorSalarioAnual(arraySalarios) {
+    let menorSalario = arraySalarios[0];
+    for (let i = 0; i < arraySalarios.length; i++) {
+        if (arraySalarios[i] < menorSalario) {
+            menorSalario = arraySalarios[i]
+        }
+    }
+    return menorSalario;
 }
 
-function calcularSalarioAnualPromedio() {
-
+function calcularSalarioAnualPromedio(arraySalarios) {
+    let contadorSalarios = 0;
+    for (let i = 0; i < arraySalarios.length; i++) {
+        contadorSalarios += arraySalarios[i];
+    }
+    return contadorSalarios / arraySalarios.length;
 }
 
-function calcularSalarioMensualPromedio() {
-
+function calcularSalarioMensualPromedio(arraySalarios) {
+    let contadorSalarios = 0;
+    for (let i = 0; i < arraySalarios.length; i++) {
+        contadorSalarios += arraySalarios[i];
+    }
+    let salarioMensualPromedio = contadorSalarios / 12;
+    return salarioMensualPromedio;
 }
 
 function crearNuevosInputs() {
     const formulario = document.querySelector('#formulario');
+    const nuevaDiv = document.createElement('div');
     const nuevoLabel = document.createElement('label');
     const nuevoInput = document.createElement('input');
+    nuevaDiv.className = 'inputs-salarios'
     nuevoInput.className = 'inputs';
-    nuevoLabel.className = 'labels'
+    nuevoLabel.className = 'labels';
     nuevoLabel.textContent = 'Ingrese salario';
-    formulario.appendChild(nuevoLabel);
-    formulario.appendChild(nuevoInput);
+    formulario.appendChild(nuevaDiv);
+    nuevaDiv.appendChild(nuevoLabel);
+    nuevaDiv.appendChild(nuevoInput);
 }
 
+function eliminarInputsCreados () { //Esta funcion borra el primer elemento y no el último
+    let nodeListInputs = document.querySelectorAll('div');
+    console.log(nodeListInputs);
+   nodeList
+}
+
+//.pop() elimina el ultimo valor de un array. Capaz puedo hacer algo asi:
+/*
+
+1. Crear un <div> por cada label e input y ponerles una clase.
+2. Elegir esa clase con querySelectorAll lo que me va a devolver un nodelist.
+3. Crear un array
+3. Luego usar el .pop() para sacar el ultimo elemento del array.
+4. Esto como se traduce en el html iguaL? solo hago el pop del array y nada más.
+
+//Si no anda, probar usar ese mismo nodelist/array .lastChild() y ver que hace.
+
+
+//ACA COMO CREAR UN ARRAY
+let edadesNodeList = document.querySelectorAll('.edades-familiares');
+        const edadesArray = [];
+        for (let i = 0; i < edadesNodeList.length; i++) {
+            edadesArray.push(Number(edadesNodeList[i].value));
+        }
+
+//ACA FUNCION VIEJA
 function eliminarInputsCreados () { //Esta funcion borra el primer elemento y no el último
     let selectorLabel = document.querySelector('.labels');
     let selectorInput = document.querySelector('.inputs');
     selectorLabel.closest('.labels').remove(); //Ver de usar "last.child" acá y ver si funciona
     selectorInput.closest('.inputs').remove();
 }
+
+
+*/
+
+
 
 //CHUSMEAR .remove() y empty()
 
@@ -59,7 +114,17 @@ $botonAgregar.onclick = function() {
 
 const $botonCalcular = document.querySelector('#boton-calcular');
 $botonCalcular.onclick = function() {
-    console.log("Hola")
+    //aca adentro crear el array de los inputs que hay
+    let salariosNodeList = document.querySelectorAll('.inputs');
+    const arraySalarios = [];
+    for (let i = 0; i < salariosNodeList.length; i++) {
+        arraySalarios.push(Number(salariosNodeList[i].value));
+    }
+    
+
+
+    //usar el array con las funciones
+    //adherir los resultadaos a los strong
 }
 
 //Agregar los calculos de los sueldos a las funciones
